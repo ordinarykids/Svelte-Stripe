e8a85ba9979bf37d29345c23e7f311d7a9c4843b
Jason Herring <jason@ordinarykids.com>
2024-03-11 12:40:11 -0700

    sloppy init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Header from './Header.svelte';
+	// import Footer from './Footer.svelte';
+
+	// import { navigating } from "$app/stores";
+	import { onMount } from 'svelte';
+
+	import '@fontsource/ibm-plex-mono/200-italic.css';
+	import '@fontsource/ibm-plex-mono/400-italic.css';
+	import '@fontsource/ibm-plex-mono/700-italic.css';
+	import '@fontsource-variable/inter';
+	import '../app.css';
+	// import { fade, fly } from 'svelte/transition';
+	// import { cubicIn, cubicOut } from 'svelte/easing';
+	// // export let data;
+
+	// import PageTransition from '../lib/transition.svelte';
+
+	export let data;
+</script>
+
+<Header />
+<!-- <PageTransition key={data.url} duration={2000}>
+    <slot />
+</PageTransition> -->
+<!-- {#key data.pathname}
+	<div
+		in:fly={{ easing: cubicOut, y: 10, duration: 300, delay: 400 }}
+		out:fly={{ easing: cubicIn, y: -10, duration: 300 }}
+	>
+		<div class="m-auto py-16 sm:w-full sm:px-4">
+			<slot />
+		</div>
+	</div>
+{/key} -->
+
+<div class="container m-auto">
+	<slot />
+</div>
+
+<!-- <Footer /> -->
